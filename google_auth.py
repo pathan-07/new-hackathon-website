909a5ab9420ee0f6e4b37c2618cce0519fca23d4
Pathan Mo. Faizan Khan <fk284099@gmail.com>
2025-03-24 19:19:46 +0530

    done

--- google_auth.py
+++ google_auth.py
@@ -2,7 +2,7 @@ import json
 import os
 import requests
 from app import db
-from flask import Blueprint, redirect, request, url_for
+from flask import Blueprint, redirect, request, url_for, session
 from flask_login import login_required, login_user, logout_user
 from models import User
 from oauthlib.oauth2 import WebApplicationClient
